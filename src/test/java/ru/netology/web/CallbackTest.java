0b38a29fe35fbb8b914460b1fa582f86a8098a5f
Larisa Karelina <karelina.rss@gmail.com>
2020-07-14 11:53:09 +0300

    initial commit

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,54 +1,31 @@
 package ru.netology.web;
 
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
-    private WebDriver driver;
-    ChromeOptions options;
-
-    @BeforeEach
-    void setUp() {
-        options = new ChromeOptions();
-        options.addArguments("--headless", "--disable-gpu", "--ignore-certificate-errors");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(options);
-    }
-
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-
-    }
 
     @Test
     void shouldSubmitRequest() {
-        driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иванов Олег");
-        elements.get(1).sendKeys("+79991992929");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button__text")).click();
+        open("http://localhost:9999");
+        SelenideElement form = $("[class=\"form form_size_m form_theme_alfa-on-white\"]");
 
-        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        form.$("[data-test-id=name] input").setValue("Иванов Олег");
+        form.$("[data-test-id=phone] input").setValue("+79991992929");
+
+        form.$("[data-test-id=agreement]").click();
+        form.$("[class=\"button__text\"]").click();
+
+        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
+
 
 
 
